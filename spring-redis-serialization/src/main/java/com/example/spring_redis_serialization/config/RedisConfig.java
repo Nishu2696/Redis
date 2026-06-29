1ca48d53cc173a163755c9911807e876ac20b80a
Nishaanth Vikram <nishaanth2696@gmail.com>
2026-06-29 10:29:38 +0530

    Using proper import for setting value serialization

--- spring-redis-serialization/src/main/java/com/example/spring_redis_serialization/config/RedisConfig.java
+++ spring-redis-serialization/src/main/java/com/example/spring_redis_serialization/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -14,15 +15,15 @@ public class RedisConfig {
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(RedisSerializer.json());
+        redisTemplate.setValueSerializer(RedisSerializer.json());
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
 }
 
 /*
-* If we don't add line 14-18, while getting the object we will get random objects
+* If we don't add line 14-18, while getting the object we will get random objects [\xac\xed\x00\x05sr...]
 * Reason being to store the data in Redis we need it in JSON format, but the data available is in Java Object
 * Hence a serializer is being used while setting the data
 * And at the fetch place we are de-serializing it back
@@ -32,10 +33,10 @@ public class RedisConfig {
             * public StringRedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
                     StringRedisTemplate<String, String> redisTemplate = new StringRedisTemplate<>();
                     redisTemplate.setConnectionFactory(redisConnectionFactory);
-                    redisTemplate.setKeySerializer(new StringRedisSerializer());
+                    redisTemplate.setKeySerializer(new StringRedisSerializer()); // Convert all the redis key into plain string
                     redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-                    redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-                    redisTemplate.setValueSerializer(new StringRedisSerializer());
+                    redisTemplate.setHashValueSerializer(RedisSerializer.json());
+                    redisTemplate.setValueSerializer(RedisSerializer.json()); // Convert Java Object into JSON and then stores it in Redis
                     redisTemplate.afterPropertiesSet();
                     return redisTemplate;
                 }
