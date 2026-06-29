02d507f1fe9ab26ffb971c7f152d3bcf8dfea842
Nishaanth Vikram <nishaanth2696@gmail.com>
2026-06-29 10:15:56 +0530

    Serializer Example

---
+++ spring-redis-serialization/src/main/java/com/example/spring_redis_serialization/SpringRedisSerializationApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_redis_serialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisSerializationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisSerializationApplication.class, args);
+	}
+
+}
---
+++ spring-redis-serialization/src/main/java/com/example/spring_redis_serialization/config/RedisConfig.java
@@ -0,0 +1,42 @@
+package com.example.spring_redis_serialization.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
+
+/*
+* If we don't add line 14-18, while getting the object we will get random objects
+* Reason being to store the data in Redis we need it in JSON format, but the data available is in Java Object
+* Hence a serializer is being used while setting the data
+* And at the fetch place we are de-serializing it back
+*
+* NOTE: Here we have used RedisTemplate ---> This is used for objects
+*       If we had only Strings to be stored in Redis at that time instead of using Redis Template we will be using StringRedisTemplate
+            * public StringRedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+                    StringRedisTemplate<String, String> redisTemplate = new StringRedisTemplate<>();
+                    redisTemplate.setConnectionFactory(redisConnectionFactory);
+                    redisTemplate.setKeySerializer(new StringRedisSerializer());
+                    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+                    redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+                    redisTemplate.setValueSerializer(new StringRedisSerializer());
+                    redisTemplate.afterPropertiesSet();
+                    return redisTemplate;
+                }
+* */
