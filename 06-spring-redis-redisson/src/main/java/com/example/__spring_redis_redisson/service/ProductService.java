6a6a4fd1d70efd76661eb78c563b011c2d1089f8
Nishaanth Vikram <nishaanth2696@gmail.com>
2026-07-02 10:26:10 +0530

    update redisson

--- 06-spring-redis-redisson/src/main/java/com/example/__spring_redis_redisson/service/ProductService.java
+++ 06-spring-redis-redisson/src/main/java/com/example/__spring_redis_redisson/service/ProductService.java
@@ -16,11 +16,18 @@ public class ProductService {
 
 //    Note here we have used lock.lock()
 //    which means a watch dog will be created which will internally set a TTL
-//    say it internally sets the TTL as 30s, so for every second this watch dog will be monitoring
+//    internally inside redisson there is a file called "internalLockLeaseTime" which has default time set as 30s, so for every second this watch dog will be monitoring
 //    after 30 s if the lock is not freed up it will extend the ttl again by 30s
 //    so in this way dynamic TTL is set
 
-//    
+//    what if the app crashes at 20sec, then in this case watchdog dies, and after 30 seconds the lock gets free, because that is the default TTL
+
+//    Now what if I need to write a custom TTL
+//    NOTE: we can create a custom TTL, but this breaks the property of redisson, which is basically, the lock release should be dynamic
+
+//    but still if we want to achieve the above requirement where we define our TTL in our redisson
+//    then the syntax looks like this: lock.lock(60, TimeUnit.seconds) here i am mentioning the lock should be of 60sec and not default 30 sec
+//    Once we define a TTL, then in that case, there will be no watchdog associated for this lock, and it will behave like a SETNX, what we discussed previously
 
     @Transactional
     public void purchase(Long productId){
